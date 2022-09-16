5a1656962d95ece5b30231e6d071d34032a56afe
hayden <haydenburger43@gmail.com>
2022-09-16 16:59:18 -0500

    added to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -5,7 +5,15 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
-
+module: {
+  rules: [
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
+  ]
+}
+import logo from '../client/favicon.ico' 
 module.exports = () => {
   return {
     mode: 'development',
