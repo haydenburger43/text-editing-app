8a45e82af621e22490453d4c823b576cfec50178
hayden <haydenburger43@gmail.com>
2022-09-16 23:44:13 -0500

    fixed pathing issues

--- Develop/server/routes/htmlRoutes.js
+++ Develop/server/routes/htmlRoutes.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const express = require('express');
+const app = express();
 
 module.exports = (app) =>
   app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../client/index.html'))
+    res.sendFile(path.join(__dirname, '../../client/index.html'))
   );
 
   app.use(express.static('../server.js'));
+
